726b2471600c314640e655d68592c89d6ef33d70
Chris Mittell <mittell@hotmail.co.uk>
2022-10-31 10:26:56 +0900

    feat: basic app setup

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'bundle.js',
+		publicPath: '/',
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							['@babel/preset-env', { targets: 'defaults' }],
+							['@babel/preset-react', { runtime: 'automatic' }],
+						],
+					},
+				},
+			},
+		],
+	},
+	devServer: {
+		hot: true,
+		port: 3000,
+		open: true,
+	},
+};
